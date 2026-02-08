81e508132190fe1d0f5e25cb4f2b2327398583f7
Julia Bessonova <bessonova.jul@gmail.com>
2026-02-08 19:30:42 +0300

    fixtures in pytest

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+from typing import Any, Generator
+
+import pytest
+from playwright.sync_api import Playwright, Page
+
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
